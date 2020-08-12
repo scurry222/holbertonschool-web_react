c8e5f22ac5860ec49c275a8b7fdcb10586dec8ee
scurry222 <scurry222@gmail.com>
2020-08-12 15:11:34 -0700

    webpack intro

---
+++ 0x01-Webpack/task_1/src/index.js
@@ -0,0 +1,5 @@
+import $ from "jquery";
+
+$('body').append('<p>Holberton Dashboard</p><br>')
+$('body').append('<p>Dashboard data for the students</p><br>')
+$('body').append('<p>Copyright - Holberton School</p>')
