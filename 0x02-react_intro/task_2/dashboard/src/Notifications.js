1b2ea64727d877aa18ab27426935849674ac5e0d
scurry222 <scurry222@gmail.com>
2020-08-18 09:12:02 -0700

    add notification component

--- 0x02-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './Notifications.css'
+import { getLatestNotification } from './utils'
 
 export const Notification = () => {
-    return (
-        <div className="Notifications">
-            <p>Here is the list of notifications</p>
-        </div>
-    )
+  return (
+    <div className="Notifications">
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority='default'>New course available</li>
+        <li data-priority='urgent'>New resume available</li>
+        <li
+        data-priority='urgent'
+        dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+      <button
+        style={{
+            display: 'inline',
+        }}
+        aria-label='Close'
+        onClick="console.log(Close button has been clicked)"
+    >
+        <img src="./close-icon.png"></img>
+      </button>
+    </div>
+  )
 }
 
 export default Notification
--- 0x02-react_intro/task_2/dashboard/src/index.js
+++ 0x02-react_intro/task_2/dashboard/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
+import Notifications from './Notifications'
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Notifications />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
